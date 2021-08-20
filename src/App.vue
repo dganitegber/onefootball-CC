f7315e21bc1b2108b83d34642feec905713859a6
dganitegber <dganite@gmail.com>
2021-08-20 11:27:11 +0200

    axios works

--- src/App.vue
+++ src/App.vue
@@ -35,18 +35,18 @@
 
     <v-main>
       <router-view />
+      <PlayerIndex />
     </v-main>
   </v-app>
 </template>
 
 <script lang="ts">
-import Vue from "vue";
-
-export default Vue.extend({
-  name: "App",
-
-  data: () => ({
-    //
-  }),
-});
+import { Component, Vue } from "vue-property-decorator";
+import PlayerIndex from "./components/PlayerIndex.vue";
+@Component({
+  components: {
+    PlayerIndex,
+  },
+})
+export default class App extends Vue {}
 </script>
---
+++ src/components/PlayerIndex.vue
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
+<template>
+  <h1>hi</h1>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import axios from "axios";
+@Component
+export default class PlayerIndex extends Vue {
+  // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+  public async created() {
+    try {
+      await axios
+        .get(
+          "https://web-sandbox.onefootball.com/assignments/player/data/fabio.json"
+        )
+        .then((res) => {
+          console.log(res.data);
+        });
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
+</script>
+
+<style></style>
