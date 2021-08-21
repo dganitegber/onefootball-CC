8e2865abb395564b2830d2a759b2acb27fe1a74f
Dganit Eger <dganit.eger@hochfrequenz.de>
2021-08-21 22:48:07 +0200

    mostly done

--- src/components/PlayerIndex.vue
+++ src/components/PlayerIndex.vue
@@ -1,28 +1,286 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 <template>
-  <h1>hi</h1>
+  <v-container class="flex-column">
+    <svg
+      xmlns:dc="http://purl.org/dc/elements/1.1/"
+      xmlns:cc="http://creativecommons.org/ns#"
+      xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+      xmlns:svg="http://www.w3.org/2000/svg"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+      xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+      version="1.1"
+      id="Layer_1"
+      x="0px"
+      y="0px"
+      viewBox="0 0 512 512"
+      style="enable-background: new 0 0 512 512"
+      xml:space="preserve"
+      sodipodi:docname="371952.svg"
+      inkscape:version="1.0.1 (3bc2e813f5, 2020-09-07)"
+    >
+      <metadata id="metadata11">
+        <rdf:RDF
+          ><cc:Work rdf:about=""
+            ><dc:format>image/svg+xml</dc:format
+            ><dc:type
+              rdf:resource="http://purl.org/dc/dcmitype/StillImage" /></cc:Work
+        ></rdf:RDF>
+      </metadata>
+      <defs id="defs9" />
+      <sodipodi:namedview
+        pagecolor="#ffffff"
+        bordercolor="#666666"
+        borderopacity="1"
+        objecttolerance="10"
+        gridtolerance="10"
+        guidetolerance="10"
+        inkscape:pageopacity="0"
+        inkscape:pageshadow="2"
+        inkscape:window-width="1920"
+        inkscape:window-height="1001"
+        id="namedview7"
+        showgrid="false"
+        inkscape:zoom="1.6210938"
+        inkscape:cx="256"
+        inkscape:cy="229.68032"
+        inkscape:window-x="-9"
+        inkscape:window-y="-9"
+        inkscape:window-maximized="1"
+        inkscape:current-layer="Layer_1"
+      />
+      <!-- <g id="g4">
+                <path
+                    d="M300.7,56L162.5,451.3h99.7L400.4,56H300.7z M481,374.3c0,45.1-36.1,81.7-80.7,81.7c-44.6,0-80.7-36.5-80.7-81.7   c0-45.1,36.1-81.7,80.7-81.7C444.7,292.6,481,329.1,481,374.3z M188.2,56c-13.3,48.4-63.2,90.4-126.3,81L31,225.2   c149.9,18.5,217.1-56,254.3-169.2H188.2L188.2,56z"
+                    id="path2"
+                /> -->
+      <!-- </g> -->
+      <g id="g24">
+        <path
+          style="fill: black; stroke: #000000; stroke-width: 0.616867"
+          d="m 59.836145,227.40441 c -7.451099,-0.3486 -26.828779,-2.1521 -27.519359,-2.56125 -1.020037,-0.60434 29.560547,-87.46091 30.68662,-87.15773 2.578886,0.69433 24.42135,0.7772 30.147814,0.11439 37.04316,-4.28761 70.32934,-27.77986 87.45038,-61.719499 2.31052,-4.580241 5.02231,-10.894545 6.02619,-14.031787 l 1.82522,-5.704075 48.19605,-0.0019 48.19606,-0.0019 -2.72582,7.86506 C 266.04704,110.58037 244.97863,146.89484 219.00778,172.98749 179.7233,212.45621 126.83105,230.53878 59.836145,227.40441 Z"
+          id="path15"
+        />
+        <path
+          style="fill: black; stroke: #000000; stroke-width: 0.616867"
+          d="m 165.68155,443.27068 c 1.52144,-4.32578 32.55682,-93.0853 68.9675,-197.24337 L 300.85029,56.648996 350.2902,56.491109 c 27.19196,-0.08684 49.43992,-0.0417 49.43992,0.100301 0,0.142003 -29.53501,84.73931 -65.63335,187.99403 -36.09834,103.25472 -67.10992,191.9691 -68.91462,197.14307 l -3.28127,9.40723 h -49.4928 -49.4928 z"
+          id="path17"
+        />
+        <path
+          style="fill: black; stroke: #000000; stroke-width: 0.616867"
+          d="m 386.5013,454.45834 c -15.25516,-2.75393 -29.96171,-10.30037 -41.25185,-21.16776 -11.67989,-11.24255 -19.05071,-24.00073 -23.0928,-39.97139 -1.55394,-6.13975 -1.68209,-7.58605 -1.68209,-18.98345 0,-11.39739 0.12815,-12.8437 1.68209,-18.98344 6.05655,-23.92998 21.35967,-43.22028 42.86446,-54.03268 18.07974,-9.09031 41.03174,-10.77764 60.00058,-4.41097 27.28164,9.15676 47.81418,32.36237 54.04424,61.08011 1.43277,6.60442 1.61332,24.56418 0.31317,31.1518 -6.63654,33.62609 -32.76172,59.61464 -65.70269,65.35906 -6.80465,1.18664 -20.4883,1.16585 -27.17511,-0.0413 z"
+          id="path19"
+        />
+      </g>
+    </svg>
+
+    <v-text-field
+      max-width="30%"
+      label="Start here"
+      v-model="searchBar"
+      @keydown.enter="searhcPlayer()"
+    ></v-text-field>
+    <v-snackbar v-model="snackbar">
+      {{ text }}
+
+      <template v-slot:action="{ attrs }">
+        <v-btn color="pink" text v-bind="attrs" @click="snackbar = false">
+          Close
+        </v-btn>
+      </template>
+    </v-snackbar>
+
+    <v-card elevation="5" max-width="fit-content" v-if="generateCard">
+      <!-- <h1>hi!</h1> -->
+      <div class="flex-column">
+        <img height="280" :src="foundPlayerExtendedProfile.profile.picture" />
+        <v-col class="flex-column">
+          <p>Name: {{ foundPlayerExtendedProfile.id }}</p>
+          <p>Active: {{ foundPlayer.active }}</p>
+          <p>Age: {{ foundPlayerExtendedProfile.profile.age }}</p>
+          <p>Role: {{ foundPlayerExtendedProfile.profile.role }}</p>
+          <p>Team: {{ foundPlayerExtendedProfile.profile.team }}</p>
+        </v-col>
+      </div>
+    </v-card>
+  </v-container>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import axios from "axios";
+export interface FoundPlayer {
+  active: boolean;
+  id: string;
+  ["profile-id"]: string;
+}
+
+export interface FoundPlayerExtendedProfile {
+  id: string;
+  profile: {
+    age: string;
+    picture: string;
+    role: string;
+    team: string;
+  };
+  stats: {
+    blocks: number;
+    totalTackles: number;
+    interceptions: number;
+    tacklesWon: number;
+    yellowCards: number;
+    passingAccuracy: number;
+    clearances: number;
+    totalCrosses: number;
+    aerialDuelsWon: number;
+    minutesPlayed: number;
+    minutesPerGoal: number;
+    headedGoals: number;
+    foulsConceded: number;
+    totalPasses: number;
+    totalShots: number;
+    otherGoals: number;
+    duelsWon: number;
+    shotsOnTarget: number;
+    leftFootGoals: number;
+    rightFootGoals: number;
+    starts: number;
+    goalsFromInsideBox: number;
+    substitutionOff: number;
+    foulsWon: number;
+    assists: number;
+    gamesPlayed: number;
+    substitutionOn: number;
+    redCards: number;
+    goals: number;
+    successfulCrosses: number;
+    goalsFromOutsideBox: number;
+  };
+}
 @Component
 export default class PlayerIndex extends Vue {
+  public foundPlayer: FoundPlayer | undefined;
+  public foundPlayerExtendedProfile: FoundPlayer | undefined;
+  public searchBar = "";
+  public generateCard = false;
+  public isPlayerActive = true;
+  public snackbar = false;
+  public text = "";
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  public async created() {
+
+  public async searhcPlayer() {
+    console.log("search", this.searchBar);
     try {
       await axios
         .get(
-          "https://web-sandbox.onefootball.com/assignments/player/data/fabio.json"
+          "https://web-sandbox.onefootball.com/assignments/player/data/" +
+            this.searchBar +
+            ".json"
         )
         .then((res) => {
-          console.log(res.data);
+          console.log("res.status", res.status);
+          this.foundPlayer = res.data;
+
+          console.log(
+            "(this.foundPlayer as any).active",
+            (this.foundPlayer as any).active
+          );
+          this.extendedSearch();
         });
     } catch (error) {
-      console.error(error);
+      console.log(error);
+      this.text = "Sorry! player not found";
+      this.snackbar = true;
+    }
+    //     console.log("this.foundPlayer", this.foundPlayer);
+    //     if ((this.foundPlayer as any).active === true) {
+    //         console.log(
+    //             "this.foundPlayer active",
+    //             (this.foundPlayer as any).active
+    //         );
+    //     } else {
+    //         console.log(
+    //             "(this.foundPlayer as any).active",
+    //             (this.foundPlayer as any).active
+    //         );
+    //         console.log("im here");
+    //         console.log("player inactive");
+    //         this.snackbar = true;
+    //     }
+    //     if (this.foundPlayer as any["profile-id"]) {
+  }
+
+  public async extendedSearch() {
+    console.log((this.foundPlayer as any).active);
+    if ((this.foundPlayer as any).active === "false") {
+      console.log("im here");
+      this.text = "Player is inactive. Sorry!";
+      this.snackbar = true;
+    } else {
+      try {
+        await axios
+          .get(
+            "https://web-sandbox.onefootball.com/assignments/player/profile/" +
+              (this.foundPlayer as any)["profile-id"]
+          )
+          .then((res) => {
+            console.log(res.data);
+            this.foundPlayerExtendedProfile = res.data;
+            console.log((this.foundPlayerExtendedProfile as any).profile);
+            this.generateCard = true;
+          });
+      } catch (error) {
+        console.log(error);
+      }
     }
   }
 }
 </script>
 
-<style></style>
+<style scopes>
+.container {
+  width: 50% !important;
+}
+.v-card {
+  display: block;
+  margin: auto;
+}
+svg {
+  width: 50%; /* you can use % */
+  /* height: auto; */
+  display: block;
+  margin: auto;
+  left: 50%;
+  animation: fadeIn 2s;
+}
+#path15 {
+  transition: 1s ease-in-out;
+  animation: slideDown 3s;
+}
+#path19 {
+  transition: 1s ease-in-out;
+  animation: slideUp 3s;
+}
+
+@keyframes slideUp {
+  0% {
+    transform: translatey(30%);
+    opacity: 0%;
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 100%;
+  }
+}
+
+@keyframes slideDown {
+  0% {
+    transform: translatey(-30%);
+    opacity: 0%;
+  }
+  100% {
+    transform: translateX(0);
+    opacity: 100%;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,16 @@
 <template>
-  <hello-world />
+  <player-index />
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "../components/HelloWorld.vue";
+import PlayerIndex from "../components/PlayerIndex.vue";
 
 export default Vue.extend({
   name: "Home",
 
   components: {
-    HelloWorld,
+    PlayerIndex,
   },
 });
 </script>
